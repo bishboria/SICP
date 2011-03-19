806129e082306933ad1349434a8b86cc5b990004
bish boria <bishboria@gmail.com>
2011-03-19 18:14:31 +0000

    Adding exercise 1.10

---
+++ exercises/ex1.10.lisp
@@ -0,0 +1,98 @@
+;; exercise 1.10
+
+(define (expect expected actual)
+  (if (equal? expected actual)
+    #t
+    actual))
+
+;; Ackermann's function
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(A 1 10)
+(A 0 (A 1 9))
+(A 0 (A 0 (A 1 8)))
+(A 0 (A 0 (A 0 (A 1 7))))
+(A 0 (A 0 (A 0 (A 0 (A 1 6)))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 32)))))
+(A 0 (A 0 (A 0 (A 0 64))))
+(A 0 (A 0 (A 0 128)))
+(A 0 (A 0 256))
+(A 0 512)
+1024
+;; so (A 1 n) -> 2^n
+
+(A 2 4)
+(A 1 (A 2 3))
+(A 1 (A 1 (A 2 2)))
+(A 1 (A 1 (A 1 (A 2 1))))
+(A 1 (A 1 (A 1 2)))
+(A 1 (A 1 (A 0 (A 1 1))))
+(A 1 (A 1 (A 0 2)))
+(A 1 (A 1 4))
+(A 1 (A 0 (A 1 3)))
+(A 1 (A 0 (A 0 (A 1 2))))
+(A 1 (A 0 (A 0 (A 0 (A 1 1)))))
+(A 1 (A 0 (A 0 (A 0 2))))
+(A 1 (A 0 (A 0 4)))
+(A 1 (A 0 8))
+(A 1 16)
+;; and by comment on line 36
+(A 0 32768)
+65536
+
+(A 3 3)
+(A 2 (A 3 2))
+(A 2 (A 2 (A 3 1)))
+(A 2 (A 2 2))
+(A 2 (A 1 (A 2 1)))
+(A 2 (A 1 2))
+(A 2 (A 0 (A 1 1)))
+(A 2 (A 0 2))
+(A 2 4)
+;; which is 
+(A 0 32768)
+65536
+
+(define (f n) (A 0 n))
+(expect 0 (f 0))
+(expect 2 (f 1))
+(expect 4 (f 2))
+(expect 6 (f 3))
+;; (f n) -> (* 2 n)
+
+(define (g n) (A 1 n))
+(expect 0 (g 0))
+(expect 2 (g 1))
+(expect 4 (g 2))
+(expect 8 (g 3))
+(expect 16 (g 4))
+;; (g n) -> 2**n
+
+(define (h n) (A 2 n))
+(expect 0 (h 0))
+(expect 2 (h 1))
+(expect 4 (h 2))
+(expect 16 (h 3))
+(expect 65536 (h 4))
+;; (h n) -> 2^2^...{n times}
+
+(define (k n) (* 5 n n))
+(expect 0 (k 0))
+(expect 5 (k 1))
+(expect 20 (k 2))
+(expect 45 (k 3))
+;; (k n) -> 5n^2
