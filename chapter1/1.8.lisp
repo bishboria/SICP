038775328654c534a5054aa6935abd2fb998c38a
bish boria <bishboria@gmail.com>
2011-03-19 12:24:23 +0000

    chapter code introducing block structure and lexical scoping

---
+++ chapter1/1.8.lisp
@@ -0,0 +1,10 @@
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+      guess
+      (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
