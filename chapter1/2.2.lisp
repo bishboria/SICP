c6475535876994c183e220348372701a5debd89b
bish boria <bishboria@gmail.com>
2011-03-22 12:58:17 +0000

    Adding Fibonacci example from start of section

--- chapter1/2.2.lisp
+++ chapter1/2.2.lisp
@@ -1,5 +1,21 @@
 ;; 1.2.2 Tree Recursion
 
+;; recursive Fibonacci
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+;; linear Fibonacci
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+    b
+    (fib-iter (+ a b) a (- count 1))))
+
 
 ;; count the number of ways that change can be made for a given amount.
 (define (count-change amount)
