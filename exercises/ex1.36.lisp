0bc12829a642a52c3640606a6897cb585708ed19
bish boria <bishboria@gmail.com>
2011-04-22 18:27:32 +0100

    extracting a print-value method to the purpose more clear

--- exercises/ex1.36.lisp
+++ exercises/ex1.36.lisp
@@ -1,13 +1,15 @@
 ;; Ex 1.36 Show the sequence of approximations using newline and display.
 ;; Calculate x -> log(1000)/log(x)
 (define tolerance 0.00001)
+(define (print-value value)
+  (newline)
+  (display next))
 (define (fixed-point f first-guess)
   (define (close-enough? v1 v2)
     (< (abs (- v1 v2)) tolerance))
   (define (try guess)
     (let ((next (f guess)))
-      (newline)
-      (display next)
+      (print-value guess)
       (if (close-enough? guess next)
         next
         (try next))))
