d331ce22883ca9e31550948cfbb90e03d6fff380
bish boria <bishboria@gmail.com>
2011-04-22 18:01:01 +0100

    Adding chapter examples for 1.3.3

---
+++ chapter1/3.3.lisp
@@ -0,0 +1,62 @@
+;; 1.3.3 Procedures as general methods
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+      midpoint
+      (let ((test-value (f midpoint)))
+        (cond ((positive? test-value)
+               (search f neg-point midpoint))
+              ((negative? test-value)
+               (search f midpoint pos-point))
+              (else midpoint))))))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+            (error "Values are not of opposite sign" a b)))))
+
+(half-interval-method sin 2.0 4.0)
+
+(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+                      1.0
+                      2.0)
+
+(half-interval-method (lambda (x) (* x x)) -1.0 1.5)
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+(fixed-point cos 0.0)
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+             1.0)
+
+;; Flips values so no good...
+(define (sqrt x)
+  (fixed-point (lambda (y) (/ x y))
+               1.0))
+
+;; Gets there by taking averages
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
