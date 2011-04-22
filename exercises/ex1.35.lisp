ab18bd93773e53e9c9614cc4ab2552f907d82159
bish boria <bishboria@gmail.com>
2011-04-22 18:06:27 +0100

    Adding exercise 1.35

---
+++ exercises/ex1.35.lisp
@@ -0,0 +1,13 @@
+;; Calculate the Golden Ratio as a fixed point of the function x -> 1 + 1/x
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define golden-ratio (fixed-point (lambda (y) (+ 1 (/ 1 y))) 1.0))
