cf317b30302120255f980284142717f894b46cc5
bish boria <bishboria@gmail.com>
2011-04-23 11:33:43 +0100

    Adding exercise 1.40

---
+++ exercises/ex1.40.lisp
@@ -0,0 +1,35 @@
+;; Ex 1.40 Define a procedure cubic that can be used in newtons-method to approximate zeros of x^3 + ax^2 + bx + c
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (cubic a b c)
+  (lambda (x)
+    (+ (cube x)
+       (* a (square x))
+       (* b x)
+       c)))
+
+(newtons-method (cubic 1 1 1) 1.0)
