c2eb8b70d01e143d2dffc72f15845f1639f9728a
bish boria <bishboria@gmail.com>
2011-04-26 18:58:15 +0100

    Adding chapter examples for 2.1.2

---
+++ chapter2/1.2.lisp
@@ -0,0 +1,8 @@
+(define (make-rat n d)
+  (cons n d))
+(define (numer x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (car x) g)))
+(define (denom x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (cdr x) g)))
