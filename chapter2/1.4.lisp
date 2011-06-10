f00081410bd25bd9d6cc2c57c7cb06a29517ea56
Stuart Gale <bishboria@gmail.com>
2011-06-10 16:45:07 +0100

    Adding chapter 2.1.4 examples

---
+++ chapter2/1.4.lisp
@@ -0,0 +1,23 @@
+;; Chapter 2.1.4; extended exercise - Interval arithmetic
+
+(define (resitance-parallel r1 r2)
+    (/ 1
+       (+ (/ 1 r1)
+          (/ 1 r2))))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
