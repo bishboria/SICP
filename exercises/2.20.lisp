f06d324c8c8cdfefcb590b4e219b38d952f284fb
Stuart Gale <bishboria@gmail.com>
2011-07-19 11:18:18 +0300

    Adding recursive version of the same exercise that uses cons.

--- exercises/2.20.lisp
+++ exercises/2.20.lisp
@@ -1,3 +1,4 @@
+;; Iterative version using append
 (define (same-parity first . rest)
   (same-parity-iter (remainder first 2) rest (list first)))
 
@@ -9,3 +10,20 @@
 
 (same-parity 1 2 3 4 5 6 7)
 (same-parity 2 3 4 5 6 7 8)
+
+
+;; Recursive version using cons
+(define (same-parity-b first . rest)
+  (define (same-parity-recursive parity remaining)
+      (cond ((null? remaining) '())
+            ((= parity (remainder (car remaining) 2))
+              (cons (car remaining) (same-parity-recursive parity (cdr remaining))))
+            (else (same-parity-recursive parity (cdr remaining)))))
+  (cons first (same-parity-recursive (remainder first 2) rest)))
+
+
+(same-parity-b 1 2 3 4 5 6 7)
+(same-parity-b 2 3 4 5 6 7 8)
+
+(same-parity 1 1 1 1 1 2 2 2 2 3 3)
+(same-parity-b 2 1 1 1 1 2 2 2 2 3 3)
