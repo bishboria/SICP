98c3fc72e10547e3ea3a00c665f097347ae9944f
Stuart Gale <bishboria@gmail.com>
2011-07-21 23:27:47 +0300

    Adding map examples for trees.

--- chapter2/2.2.lisp
+++ chapter2/2.2.lisp
@@ -14,3 +14,21 @@
 
 (count-leaves x)
 (count-leaves (list x x))
+
+
+(define (scale-tree tree factor)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (* tree factor))
+        (else (cons (scale-tree (car tree) factor)
+                    (scale-tree (cdr tree) factor)))))
+
+(scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
+
+(define (scale-tree-map tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+           (scale-tree-map sub-tree factor)
+           (* sub-tree factor)))
+       tree))
+
+(scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
