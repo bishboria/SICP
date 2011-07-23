96a6a84e999ae6bb514b49f44c7c8f4c45bec502
Stuart Gale <bishboria@gmail.com>
2011-07-23 19:27:28 +0300

    Adding exercise 2.35

---
+++ exercises/2.35.lisp
@@ -0,0 +1,38 @@
+;; redefine count-leaves as an accumulation
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+;; definition that uses enumerate-tree
+(define (count-leaves tree)
+  (accumulate +
+              0
+              (map (lambda (x) 1)
+                   (enumerate-tree tree))))
+
+(define t1 (list 1 (list 2 (list 3 4) 5)))
+(define t2 (list (list 1 2 3 4) (list 1 2 3 4)))
+(count-leaves t1)
+(count-leaves t2)
+
+;; recursive definition
+(define (count-leaves tree)
+  (accumulate +
+              0
+              (map (lambda (sub-tree)
+                     (if (pair? sub-tree)
+                       (count-leaves sub-tree)
+                       1))
+                   tree)))
+
+(count-leaves t1)
+(count-leaves t2)
