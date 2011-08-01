261ec7e6070b645a4859c55a7ef80a5e4d322f65
Stuart Gale <bishboria@gmail.com>
2011-08-01 22:51:20 +0300

    Adding more chapter examples and copying ex 2.44 here too.

--- chapter2/2.4.lisp
+++ chapter2/2.4.lisp
@@ -26,7 +26,38 @@
         (beside (below painter top-left)
                 (below bottom-right corner))))))
 
-(define (square-limit painte n)
+(define (square-limit painter n)
   (let ((quarter (corner-split painter n)))
     (let ((half (besidne (flip-horiz quarter) quarter)))
       (below (flip-vert half) half))))
+
+(define (up-split painter n)
+    (if (= n 0)
+          painter
+              (let ((smaller (up-split painter (- n 1))))
+                      (below (beside smaller smaller) painter))))
+
+; higher order operations
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (t1 painter) (tr painter)))
+          (bottom (beside (b1 painter) (br painter))))
+      (below bottom top))))
+
+; redefining flipped-pairs
+(define (flipped-pairs painter)
+  (let ((combine4 (square-of-four identity flip-vert
+                                  identity flip-vert)))
+    (combine4 painter)))
+; or
+(define flipped-pairs (square-of-four identity flip-vert
+                                      identity flip-vert))
+
+; redefining square-limit
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+                                  rotate180 flip-vert)))
+    (combine4 (corner-split painter n))))
+; where
+(define (rotate180 painter)
+  (flip-vert (flip-horiz painter)))
