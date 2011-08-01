97d1e3fdad24adf1ffc5a9defb2009439070de90
Stuart Gale <bishboria@gmail.com>
2011-08-01 21:12:32 +0300

    Adding examples from chapter 2.2.4

---
+++ chapter2/2.4.lisp
@@ -0,0 +1,32 @@
+;; A Picture Language
+
+(define wave2 (beside wave (flip-vert wave)))
+(define wave4 (below wave2 wave2))
+
+(define (flipped-pairs painter)
+  (let ((painter2 (beside painter (flip-vert painter))))
+    (below painter2 painter2)))
+
+(define wave4 (flipped-pairs wave))
+
+(define (right-split painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (right-split painter (- n 1))))
+      (beside painter (below smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+    painter
+    (let ((up (up-split painter (- n 1)))
+          (right (right-split painter (- n 1))))
+      (let ((top-left (beside up up))
+            (bottom-right (below right right))
+            (corner (corner-split painter (- n 1))))
+        (beside (below painter top-left)
+                (below bottom-right corner))))))
+
+(define (square-limit painte n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (besidne (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
