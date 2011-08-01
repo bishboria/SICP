a04c00684084e2163ce36596b2cec581e339388b
Stuart Gale <bishboria@gmail.com>
2011-08-01 23:28:57 +0300

    Adding ex 2.45

--- exercises/2.45.lisp
+++ exercises/2.45.lisp
@@ -2,3 +2,10 @@
 
 (define right-split (split beside below))
 (define up-split (split below beside))
+
+(define (split op1 op2)
+  (lambda (painter n)
+    (if (= n 0)
+      painter
+      (let ((smaller ((split op1 op2) painter (- n 1))))
+        (op1 painter (op2 smaller smaller))))))
