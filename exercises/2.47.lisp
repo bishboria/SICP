8feab98426857c9cdd5b668c7d34c35d5be3f006
Stuart Gale <bishboria@gmail.com>
2011-08-03 22:05:14 +0300

    Adding exercise 2.47

---
+++ exercises/2.47.lisp
@@ -0,0 +1,26 @@
+;; given two possible constructors, define the appropriate selectors
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame f)
+  (car f))
+
+(define (edge1-frame f)
+  (cadr f))
+
+(define (edge2-frame f)
+  (caddr f))
+
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (origin-frame f)
+  (car f))
+
+(define (edge1-frame f)
+  (cadr f))
+
+(define (edge2-frame f)
+  (cddr f))
