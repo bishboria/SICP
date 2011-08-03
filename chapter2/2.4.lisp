5ee9cfd6190e867d36c538fd4174c0f8ff7a5433
Stuart Gale <bishboria@gmail.com>
2011-08-03 21:42:13 +0300

    Adding chapter examples

--- chapter2/2.4.lisp
+++ chapter2/2.4.lisp
@@ -61,3 +61,12 @@
 ; where
 (define (rotate180 painter)
   (flip-vert (flip-horiz painter)))
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+      (origin-frame frame)
+      (add-vect (scale-vect (xcor-vect v)
+                            (edge1-frame frame))
+                (scale-vect (ycor-vect v)
+                            (edge2-frame frame))))))
