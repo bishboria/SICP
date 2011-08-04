22c11776860c03adb012f26769f6685c95b65b0a
Stuart Gale <bishboria@gmail.com>
2011-08-04 15:36:07 +0300

    Completing exercise 2.49 with the wave painter and roughly mapped out values in the unit square.

--- exercises/2.49.lisp
+++ exercises/2.49.lisp
@@ -31,4 +31,44 @@
                            (make-segment mt mr)
                            (make-segment mr mb))))
 
-;; the wave painter
+;; the wave painter. Starting at the leg on the bottom LHS.
+(let ((zero (make-vect 0 0.25)) ;; start LHS
+      (one (make-vect 0.37 0.5))
+      (two (make-vect 0.4 0.6))
+      (three (make-vect 0.25 0.4))
+      (four (make-vect 0 0.8))
+      (five (make-vect 0 0.9))
+      (six (make-vect 0.25 0.6))
+      (seven (make-vect 0.3 0.7))
+      (eight (make-vect 0.4 0.7))
+      (nine (make-vect 0.37 0.9))
+      (ten (make-vect 0.4 1)) ;; end LHS
+      (eleven (make-vect 0.6 1)) ;; start RHS
+      (twelve (make-vect 0.63 0.9))
+      (thirteen (make-vect 0.6 0.7))
+      (fourteen (make-vect 0.67 0.7))
+      (fifteen (make-vect 1 0.4))
+      (sixteen (make-vect 1 0.25))
+      (seventeen (make-vect 0.6 0.5))
+      (eighteen (make-vect 0.75 0)) ;; end RHS
+      (nineteen (make-vect 0.62 0)) ;; start under legs
+      (twenty (make-vect 0.5 0.35))
+      (twenty-one (make-vect 0.38 0))) ;; end under legs
+  (segments->painter (list
+                       (make-segment zero one)
+                       (make-segment one two)
+                       (make-segment two three)
+                       (make-segment three four)
+                       (make-segment five six)
+                       (make-segment six seven)
+                       (make-segment seven eight)
+                       (make-segment eight nine)
+                       (make-segment nine ten)
+                       (make-segment eleven twelve)
+                       (make-segment twelve thirteen)
+                       (make-segment thirteen fourteen)
+                       (make-segment fourteen fifteen)
+                       (make-segment sixteen seventeen)
+                       (make-segment seventeen eighteen)
+                       (make-segment nineteen twenty)
+                       (make-segment twenty twenty-one))))
