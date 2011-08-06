68ac4e527bd70acb2c201dce36a7a921dc0ab482
Stuart Gale <bishboria@gmail.com>
2011-08-06 14:01:31 +0300

    Updating 2.50 to show alternative implementations, based on 2.51's reuse

--- exercises/2.50.lisp
+++ exercises/2.50.lisp
@@ -15,3 +15,22 @@
                      (make-vect 1.0 0.0)
                      (make-vect 0.0 0.0)
                      (make-vect 1.0 1.0)))
+; or
+(define (rotate180 painter)
+  (rotate90 (rotate90 painter)))
+
+(define (rotate270 painter)
+  (rotate90 (rotate90 (rotate90 painter))))
+
+; or
+(define (repeat proc n)
+  (if (= n 1)
+    proc
+    (lambda (x)
+      (proc ((repeat proc (- n 1)) x)))))
+
+(define (rotate180 painter)
+  ((repeat rotate90 2) painter))
+
+(define (rotate270 painter)
+  ((repeat rotate90 3) painter))
