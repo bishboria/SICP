0bae7d8189fb78b343fe0a3280b72d70a32bceba
Stuart Gale <bishboria@gmail.com>
2011-08-07 14:53:13 +0300

    Adding exercise 2.55

---
+++ exercises/2.55.lisp
@@ -0,0 +1,4 @@
+(car ''abracadabra)
+; this returns quote because ' is syntactic sugar for the special form
+; procedure (quote ...). So we are calling (quote (quote abracadabra))
+; hence, (car (quote (quote abracadabra))) -> quote
