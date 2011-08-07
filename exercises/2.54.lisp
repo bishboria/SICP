651f961e8e149eb9484a65bc59bd65d166da1546
Stuart Gale <bishboria@gmail.com>
2011-08-07 14:53:01 +0300

    Adding exercise 2.54

---
+++ exercises/2.54.lisp
@@ -0,0 +1,13 @@
+(define (my-equal? a b)
+  (cond ((and (not (pair? a))
+              (not (pair? b)))
+           (eq? a b))
+        ((and (pair? a)
+              (pair? b))
+           (and (my-equal? (car a) (car b))
+                (my-equal? (cdr a) (cdr b))))
+        (else #f)))
+
+(my-equal? '(this is a list) '(this is a list))
+(my-equal? '(this is a list) '(this (is a) list))
+(my-equal? '(this (is a) list) '(this (is a) list))
