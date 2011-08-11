b4cea168a37c44c5d9156cdbfbaf564cf87c9b91
Stuart Gale <bishboria@gmail.com>
2011-08-11 21:04:51 +0300

    Adding exercise 2.61

---
+++ exercises/2.61.lisp
@@ -0,0 +1,9 @@
+; adjoin-set using ordered lists
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+(adjoin-set 3 '(1 2 4 5))
