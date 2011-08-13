54f4a84b2b5dc5711df62bc1182afb03ae9c1c22
Stuart Gale <bishboria@gmail.com>
2011-08-13 21:08:35 +0300

    Adding new chapter examples for 2.3.3

--- chapter2/3.3.lisp
+++ chapter2/3.3.lisp
@@ -67,3 +67,10 @@
          (make-tree (entry set)
                     (left-branch set)
                     (adjoin-set x (right-branch set))))))
+
+; Sets and information retrieval
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) false)
+        ((equal? given-key (key (car set-of-records)))
+         (car set-of-records))
+        (else (lookup given-key (cdr set-of-records)))))
