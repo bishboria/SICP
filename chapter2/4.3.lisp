97aa954b017cedc1dc5a18e2164623d72cd2573f
Stuart Gale <bishboria@gmail.com>
2011-08-20 21:26:30 +0300

    Adding message passing chapter example in 2.4.3

--- chapter2/4.3.lisp
+++ chapter2/4.3.lisp
@@ -69,3 +69,16 @@
   ((get 'make-from-real-imag 'rectangular) x y))
 (define (make-from-mag-ang r a)
   ((get 'make-from-mag-ang 'polar) r a))
+
+; messaging
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else (error "Unknown op -- MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
+(define (apply-generic op arg) (arg op))
