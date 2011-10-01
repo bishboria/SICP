dd2dc5b4184102e65436d5db793bd6863663d013
Stuart Gale <bishboria@gmail.com>
2011-10-01 15:26:14 +0100

    Adding chapter 3.3.1

--- chapter3/3.1.lisp
+++ chapter3/3.1.lisp
@@ -16,3 +16,34 @@ z2
 ; use eq? to detect sharing in list structures
 (eq? (car z1) (cdr z1))
 (eq? (car z2) (cdr z2))
+
+
+; immutable version
+(define (cons x y)
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+	  ((eq? m 'cdr) y)
+	  (else (error "Undefined operation -- CONS" m))))
+  dispatch)
+(define (car z) (z 'car))
+(define (cdr z) (z 'cdr))
+
+; mutable version
+(define (cons x y)
+  (define (set-x! v) (set! x v))
+  (define (set-y! v) (set! y v))
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+	  ((eq? m 'cdr) y)
+	  ((eq? m 'set-car!) set-x!)
+	  ((eq? m 'set-cdr!) set-y!)
+	  (else (error "Undefined operation -- CONS" m))))
+  dispatch)
+(define (car z) (z 'car))
+(define (cdr z) (z 'cdr))
+(define (set-car! z new-value)
+  ((z 'set-car!) new-value)
+  z)
+(define (set-cdr! z new-value)
+  ((z 'set-cdr!) new-value)
+  z)
