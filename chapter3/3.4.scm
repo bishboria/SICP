777a0d701b5afb194592dc980b58c1ef121ff6dc
Stuart Gale <bishboria@gmail.com>
2012-04-17 17:37:16 +0100

    Adding 3.3.4 so far.

---
+++ chapter3/3.4.scm
@@ -0,0 +1,61 @@
+(define a (make-wire))
+(define b (make-wire))
+(define c (make-wire))
+
+(define d (make-wire))
+(define e (make-wire))
+(define f (make-wire))
+
+; components used to make a half adder
+(or-gate a b d)
+(and-gate a b c)
+(inverter c e)
+(and-gate d e s)
+
+(define (half-adder a b s c)
+  (let ((d (make-wire))
+	(e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+	(c1 (make-wire))
+	(c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+		   (lambda ()
+		     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+	((= s 1) 0)
+	(else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+	    (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+		   (lambda ()
+		     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a1 and-action-procedure)
+  'ok)
+
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+	((or (= s1 0) (= s2 0)) 0)
+	(else (error "Invalid signal" s1 s2))))
